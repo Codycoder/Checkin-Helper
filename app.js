1b50d941b382b99d7c4cc6bdce13c6cca0ef0d82
Olivia Smith <Olivia.Smith.31196@gmail.com>
2020-03-12 08:59:25 -0400

    it worksgit add .git add .

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-let listOfNames = ["Olivia", "Hetal", "Brian"];
+let listOfNames = ["Olivia", "Hetal", "Brian", "Mercy", "Ben", "Noah", "Nick", "Jonathan", "Abdul", "Anna", "Aaron", "Stuart", "Innocent", "Fatuma", "Brandon", "Gaelan", "Al", "Daniel", "Colin", "James", "Nadir", "Daly"];
 let button = document.querySelector(".button");
 let nameList = document.querySelector(".nameList");
 let name = document.querySelector(".name");
 let randomNumber = 0;
 let randomName = "";
 
-let randomizeNumber = (randomNumber) => {
-    randomNumber = Math.floor(Math.random() * (listOfNames.length));
+// let randomizeNumber = (randomNumber) => {
+//     randomNumber = Math.floor(Math.random() * (listOfNames.length-1));
+// }
+
+let randomizeArray = () => {
+    listOfNames.sort(() => Math.random() -0.5);
 }
 
-let printName = (name, randomNumber) => {
-    randomName = listOfNames[randomNumber];
-    name.innerHTML = randomName;
+let printName = (name, randomName, listOfNames) => {
+    randomName = listOfNames[listOfNames.length-1];
+    name.innerHTML = 'Next is ' + randomName;
 }
 
-let removeName = (randomNumber) => {
-    listOfNames.splice(randomNumber, 1);
+let removeName = (listOfNames) => {
+    listOfNames.splice(listOfNames.length-1, 1);
 }
 
 let showNameList = (listOfNames) => {
@@ -25,9 +29,10 @@ let showNameList = (listOfNames) => {
 let runApp = () => {
     showNameList(listOfNames);
     button.addEventListener('click', () => {
-        randomizeNumber();
-        printName(randomNumber, randomName);
-        removeName(randomNumber);
+        // randomizeNumber(randomNumber);    
+        randomizeArray();
+        printName(name, randomName, listOfNames);
+        removeName(listOfNames);
         showNameList(listOfNames);
     })
 } 
